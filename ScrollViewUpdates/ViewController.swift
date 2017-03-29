07f1caee46848e2ff8f5a7f0fa8f23b81d4af538
szilardinho <szilardinhog@gmail.com>
2017-03-29 19:54:02 -0400

    first commit scrollView updates

--- ScrollViewUpdates/ViewController.swift
+++ ScrollViewUpdates/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    var contentWidth: CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        
 
 
 }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for index in 0...2 {
+            
+            let image = UIImage(named: "icon\(index).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX : CGFloat = 0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(index)
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: scrollView.frame.midY - 75, width: 150, height: 150 )
+            
+            
+        }
+        //scrollView.backgroundColor = UIColor.purple
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+    }
+}
